2d0bb857f81494d04b6cfc9798f514fc302001b6
Jortega23 <jortegs23@gmail.com>
2020-04-11 14:12:23 -0700

    Routes
    Added files with intial routes

---
+++ routes/contacts.js
@@ -0,0 +1,36 @@
+//Contacts CRUD routes
+
+const express = require('express')
+const router = express.Router();
+
+
+// @route    GET api/contacts
+//@desc    GET all users contacts
+// @access    Private 
+router.get('/', (req, res) => { 
+    res.send('Get all contacts')}
+    )
+
+
+// @route    POST api/contacts
+//@desc    Add new contact
+// @access    Private 
+router.post('/', (req, res) => { 
+    res.send('Add Contact')}
+    )
+
+// @route    PUT api/contacts
+//@desc      Update contact
+// @access    Private 
+router.put('/:id', (req, res) => { 
+    res.send('update contact')}
+    )
+
+ // @route    DELETE api/contacts
+//@desc      Delete contact
+// @access    Private 
+router.delete('/:id', (req, res) => { 
+    res.send('delete contact')}
+    )
+
+module.exports = router
